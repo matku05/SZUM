dc6ca702e114cef7752387c62b9cdc73636c670a
matku05 <mate.kukla@gmail.com>
2018-01-15 14:37:07 +0100

    Initial project commit

---
+++ arduinoDriver.ino
@@ -0,0 +1,257 @@
+//
+// Limit Switches
+//
+#define X_MIN_PIN           3
+#ifndef X_MAX_PIN
+  #define X_MAX_PIN         2
+#endif
+#define Y_MIN_PIN          14
+#define Y_MAX_PIN          15
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+
+
+//
+// Steppers
+//
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
+#define X_CS_PIN           53
+
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
+#define Y_CS_PIN           49
+
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+#define Z_CS_PIN           40
+
+/************ custom variables *****************/
+
+int buttonX;
+int buttonY;
+int buttonZ;
+
+char buforstring[30];
+int bufori = 0;
+char bufor = ' ';
+char axis = ' ';
+
+char dir = ' ';
+char dirX = ' ';
+char dirY = ' ';
+char dirZ = ' ';
+
+long numberOfSteps = 0;
+long numberOfStepsX = 0;
+long numberOfStepsY = 0;
+long numberOfStepsZ = 0;
+
+bool newCommand = 0;
+
+/************ setup *****************/
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(X_MAX_PIN,INPUT);
+  digitalWrite(X_MAX_PIN,HIGH);
+  pinMode(Y_MAX_PIN,INPUT);
+  digitalWrite(Y_MAX_PIN,HIGH);
+  pinMode(Z_MAX_PIN,INPUT);
+  digitalWrite(Z_MAX_PIN,HIGH);
+
+  pinMode(X_STEP_PIN,OUTPUT);
+  digitalWrite(X_STEP_PIN,LOW);
+  pinMode(X_DIR_PIN,OUTPUT);
+  digitalWrite(X_DIR_PIN,LOW);
+  pinMode(X_ENABLE_PIN,OUTPUT);
+  digitalWrite(X_ENABLE_PIN,LOW);
+  pinMode(X_CS_PIN,OUTPUT);
+  digitalWrite(X_CS_PIN,HIGH);
+
+  pinMode(Y_STEP_PIN,OUTPUT);
+  digitalWrite(Y_STEP_PIN,LOW);
+  pinMode(Y_DIR_PIN,OUTPUT);
+  digitalWrite(Y_DIR_PIN,LOW);
+  pinMode(Y_ENABLE_PIN,OUTPUT);
+  digitalWrite(Y_ENABLE_PIN,LOW);
+  pinMode(Y_CS_PIN,OUTPUT);
+  digitalWrite(Y_CS_PIN,HIGH);
+
+  pinMode(Z_STEP_PIN,OUTPUT);
+  digitalWrite(Z_STEP_PIN,LOW);
+  pinMode(Z_DIR_PIN,OUTPUT);
+  digitalWrite(Z_DIR_PIN,LOW);
+  pinMode(Z_ENABLE_PIN,OUTPUT);
+  digitalWrite(Z_ENABLE_PIN,LOW);
+  pinMode(Z_CS_PIN,OUTPUT);
+  digitalWrite(Z_CS_PIN,HIGH);
+
+  Serial.begin(9600);
+  bufori = 0;
+}
+
+/************ main loop ****************************************************************/
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // odczyt krancowek
+  buttonX = digitalRead(X_MAX_PIN);
+  buttonY = digitalRead(Y_MAX_PIN);
+  buttonZ = digitalRead(Z_MAX_PIN);
+
+  /********** odczyt komendy *****************/
+
+  while( Serial.available() )
+  {
+    bufor = Serial.read();
+    if( bufor!='\n' )
+    {
+        buforstring[ bufori ] = bufor;
+        bufori++;      
+    }
+    else
+    {
+      //Serial.print(buforstring);
+
+      sscanf( buforstring, "%c%c%ld\n",&axis,&dir,&numberOfSteps );
+
+      for(int k=0;k<30;k++)
+      {
+        buforstring[k] = '\0';  
+      }
+
+/*      Serial.print('\n');
+      Serial.println(axis);
+      Serial.println(dir);
+      Serial.println(numberOfSteps);
+*/
+      if (axis == 'x')
+        {
+          numberOfStepsX = numberOfSteps;
+          dirX = dir;
+        }
+        
+      else if (axis == 'y')
+        {
+          numberOfStepsY = numberOfSteps;
+          dirY = dir;
+        }
+      else if (axis == 'z')
+        {
+          numberOfStepsZ = numberOfSteps;
+          dirZ = dir;
+        }
+      
+      bufori = 0;
+      newCommand = 1;
+    }
+  }
+
+  /************ interpretacja ***************/
+  
+    
+      if (numberOfStepsX >0)
+      {
+        numberOfStepsX--;
+        
+        // change direction
+        if(dirX=='d')
+          digitalWrite(X_DIR_PIN,HIGH);
+        else 
+          // if dir is not 'd', set it to up
+          digitalWrite(X_DIR_PIN,LOW);
+        
+        if(!buttonX)
+        {
+          // move only if krancowka is not pressed
+          digitalWrite(X_STEP_PIN,HIGH);
+        }
+        else
+        {
+          // go down until krancowka is no longer pressed
+          digitalWrite(X_DIR_PIN,HIGH);
+          digitalWrite(X_STEP_PIN,HIGH);
+
+          // go down another 100 steps after krancowka is released
+          numberOfStepsX = 100;
+          dirX = 'd';
+        }
+        
+      }
+    
+    if (numberOfStepsY >0)
+      {
+        numberOfStepsY--;
+        
+        // change dir
+        if(dirY=='d')
+          digitalWrite(Y_DIR_PIN,HIGH);
+        else 
+          digitalWrite(Y_DIR_PIN,LOW);
+    
+        // move only if krancowka is not pressed
+        if(!buttonY)
+        {
+          digitalWrite(Y_STEP_PIN,HIGH);
+        }
+        else
+        {
+          digitalWrite(Y_DIR_PIN,HIGH);
+          digitalWrite(Y_STEP_PIN,HIGH);
+          
+          numberOfStepsY = 100;
+          dirY = 'd';
+        }
+        
+      }
+  
+    
+    if (numberOfStepsZ >0)
+      {
+        numberOfStepsZ--;
+        
+        // change dir
+        if(dirZ=='d')
+          digitalWrite(Z_DIR_PIN,HIGH);
+        else 
+          digitalWrite(Z_DIR_PIN,LOW);
+    
+        // move only if krancowka is not pressed
+        if(!buttonZ)
+        {
+          digitalWrite(Z_STEP_PIN,HIGH);
+        }
+        else
+        {
+          digitalWrite(Z_DIR_PIN,HIGH);
+          digitalWrite(Z_STEP_PIN,HIGH);
+          
+          numberOfStepsZ = 100;
+          dirZ = 'd';
+        }
+        
+      } 
+
+      delayMicroseconds(400);
+
+      digitalWrite(X_STEP_PIN,LOW);
+      digitalWrite(Y_STEP_PIN,LOW);
+      digitalWrite(Z_STEP_PIN,LOW);
+    
+      delayMicroseconds(400);
+
+      if (numberOfStepsX == 0 && numberOfStepsY == 0 && numberOfStepsZ == 0 && newCommand == 1)
+      {
+        Serial.println("OK");
+        newCommand = 0;
+      }
+
+}
+
+
+
---
+++ matlabSerial.m
@@ -0,0 +1,124 @@
+if ~exist('s1','var')
+    s1 = serial('/dev/ttyUSB0','baudrate',9600);
+    fopen(s1)
+end
+
+% centre
+% xc = 5;
+% yc = 11;
+% zc = 15;
+% r = 21.5;
+
+xc = 7.1447;
+yc = 8.25;
+zc = 10;
+r = 21.5;
+
+% columns coords
+x1 = 0;
+y1 = 0;
+x2 = 0;
+y2 = 16.5;
+x3 = 14.2894;
+y3 = 8.25;
+
+% engines positions
+z1 = zc + sqrt(r^2 - (x1-xc)^2 - (y1-yc)^2 );
+z2 = zc + sqrt(r^2 - (x2-xc)^2 - (y2-yc)^2 );
+z3 = zc + sqrt(r^2 - (x3-xc)^2 - (y3-yc)^2 );
+
+figure(1)
+cla;
+
+% columns
+col1 = line([x1 x1], [y1 y1], [0 60]);
+col2 = line([x2 x2], [y2 y2], [0 60]);
+col3 = line([x3 x3], [y3 y3], [0 60]);
+
+% arms
+arm1 = line([x1 xc], [y1 yc], [z1 zc]);
+arm2 = line([x2 xc], [y2 yc], [z2 zc]);
+arm3 = line([x3 xc], [y3 yc], [z3 zc]);
+
+view(18,36)
+
+hold on
+
+% mini animka
+for t=0:0.05:40
+    % remember last position
+    z1l = z1;
+    z2l = z2;
+    z3l = z3;
+    
+    % centre
+    xc = 5 + sin(t);
+    yc = 11 + cos(t);
+    zc = 15 + 0.01*t;
+    
+    plot3(xc,yc,zc,'.')
+    
+    % engines positions
+    z1 = zc + sqrt(r^2 - (x1-xc)^2 - (y1-yc)^2 );
+    z2 = zc + sqrt(r^2 - (x2-xc)^2 - (y2-yc)^2 );
+    z3 = zc + sqrt(r^2 - (x3-xc)^2 - (y3-yc)^2 );
+    
+    % calculate difference
+    if t>0
+       z1dif = z1 - z1l;
+       z2dif = z2 - z2l;
+       z3dif = z3 - z3l;
+       
+       % first engine
+       if z1dif < 0 
+            tekst1 = ['xd' num2str(round(z1dif*-797))];
+       else
+           tekst1 = ['xu' num2str(round(z1dif*797))];
+       end
+       
+       % second engine
+       if z2dif < 0 
+            tekst2 = ['yd' num2str(round(z2dif*-797))];
+       else
+           tekst2 = ['yu' num2str(round(z2dif*797))];
+       end
+       
+       % third engine
+       if z3dif < 0 
+            tekst3 = ['zd' num2str(round(z3dif*-797))]; 
+       else
+           tekst3 = ['zu' num2str(round(z3dif*797))]; 
+       end
+             
+       % send commands to printer
+       % [tekst1 tekst2 tekst3]
+       
+       if t>0 
+           while(s1.BytesAvailable == 0)
+               ;
+           end
+
+           while(s1.BytesAvailable)
+               fscanf(s1);
+           end
+       end
+       
+       fprintf(s1,tekst1)
+       fprintf(s1,tekst2)
+       fprintf(s1,tekst3)
+       
+    end   
+    
+    % update arms
+    set(arm1,'xdata',[x1 xc],'ydata',[y1 yc],'zdata',[z1 zc])
+    set(arm2,'xdata',[x2 xc],'ydata',[y2 yc],'zdata',[z2 zc])
+    set(arm3,'xdata',[x3 xc],'ydata',[y3 yc],'zdata',[z3 zc])
+    
+    axis([0 60 0 60 0 60])
+    axis equal
+    pause(.2)
+end
+
+
+
+
